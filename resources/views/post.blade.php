4b7bd85355b65c86d19a66a1212d1c8ed8cfc163
Ubuntu <ubuntu@ip-172-31-87-119.ec2.internal>
2023-10-15 16:14:52 +0000

    v1

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/test.css">
+<body>
+<h1>Hello world</h1>
+
+<article>
+<h1>first post</h1>
+<p>
+  I don't konw what I want to say, maybe today is a good day.
+</p>
+</article>
+</body>
