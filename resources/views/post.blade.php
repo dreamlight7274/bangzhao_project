9e370661844adf0b32cbb59f1be162262846d41b
Bangzhao Wang <dreamlight7274@gmail.com>
2023-11-14 17:04:50 +0000

    css combine

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,18 +2,27 @@
     @include('_posts_header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts_in_html->count())
         <x-post_card_horizon :post="$posts_in_html[0]" />
 
+            @if ($posts_in_html->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts_in_html->skip(1) as $post)
+                        <x-post_card :post="$post" class="bg-red-500"/>
+                    @endforeach  
 
-        {{-- <div class="lg:grid lg:grid-cols-2">
-            <x-post_card />
-            <x-post_card />
+                </div>
+            @endif
+        @else
+            <p class="text-center"> Sorry, there is no post now.</p>
 
+        @endif
+        
 
             
-        </div>
+        
 
-        <div class="lg:grid lg:grid-cols-3">
+        {{--<div class="lg:grid lg:grid-cols-3">
         <x-post_card />
         <x-post_card />
         <x-post_card />
