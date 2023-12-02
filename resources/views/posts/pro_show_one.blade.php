8e1bac2c6b19314b4bddb5d18221e4ebaf9c2ef6
Bangzhao Wang <dreamlight7274@gmail.com>
2023-12-02 23:44:10 +0000

    post-page

--- resources/views/posts/pro_show_one.blade.php
+++ resources/views/posts/pro_show_one.blade.php
@@ -1,35 +1,9 @@
 <x-layout_pro>
     <section class="px-6 py-8">
+        <main class="max-w-6xl mx-auto my-20">
+            <article class="mx-auto grid grid-cols-12 ">
+                    <div class="col-span-3">
 
-
-        <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
-            <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
-                <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                    {{-- <img src="/storage/{{ $post_in_html->thumbnail }}" alt="" class="rounded-xl"> --}}
-                    @if ($post_in_html->thumbnail)
-                    <img src="{{ asset('storage/' . $post_in_html->thumbnail) }}" alt="" class="rounded-xl">
-                    @else
-                    <img src="/images/illustration-3.png" alt="" class="rounded-xl">
-                    @endif
-                    <p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>{{ $post_in_html->created_at->diffForHumans() }}</time>
-                    </p>
-
-                    <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
-                        <div class="ml-3 text-left">
-                            <h5 class="font-bold">
-                                {{-- {{ $post_in_html->user->username }} --}}
-                                <a href="/post/?author={{ $post_in_html->user->username }}">{{ $post_in_html->user->username }}</a>
-                            
-                            </h5>
-                            <h6>Mascot at Laracasts</h6>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-span-8">
-                    <div class="hidden lg:flex justify-between mb-6">
                         <a href="/post"
                             class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
                             <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
@@ -45,73 +19,49 @@
                             Back to Posts
                         </a>
 
+                        @if ($post_in_html->user->portrait)
+                        <img src="{{ asset('storage/' . $post_in_html->user->portrait) }}" alt="portrait" class="rounded-xl">
+                        @else
+                        <img src="/images/default_portrait.png" alt="protrait" class="rounded-xl">
+                        @endif
+                        <div class="text-left">
+                            <h5 class="font-bold">
+                                {{-- {{ $post_in_html->user->username }} --}}
+                                <a href="/post/?author={{ $post_in_html->user->username }}">{{ $post_in_html->user->username }}</a>
+                            
+                            </h5>
+                        </div>
+                        <p class="mt-4 block text-gray-400 text-xs">
+                            Published <time>{{ $post_in_html->created_at->diffForHumans() }}</time>
+                        </p>
+
+
                     </div>
-
-                    <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                        {{ $post_in_html->title }}
-                    </h1>
-
-                    <div class="space-y-4 lg:text-lg leading-loose space-y-4">
-                        {!! $post_in_html->body !!}
-
+                    <div class="col-span-9">
+                        @if ($post_in_html->thumbnail)
+                    <img src="{{ asset('storage/' . $post_in_html->thumbnail) }}" alt="" class="rounded-xl">
+                    @else
+                    <img src="/images/default_post.jpg" alt="the thumbnail" class="rounded-xl">
+                    @endif
                     </div>
-                </div>
-                <section class="col-span-12 mt-10">
-                    @auth
+                    <section class="col-span-12">
+                        <h1 class="font-bold text-3xl lg:text-4xl mb-10">
+                            {{ $post_in_html->title }}
+                        </h1>
+    
+                        <div class="space-y-4 lg:text-lg leading-loose space-y-4">
+                            {!! $post_in_html->body !!}
+    
+                        </div>
 
-                    <form method="POST" action="/post/{{ $post_in_html->id }}/comment" class="py-6 rounded-xl border"> 
-                        @csrf
+                    </section>
 
-                        <header class="flex items-center"> 
-
-                            <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="pravatar" width="40" height="40"> 
-
-                            <h1 class="ml-3">   
-                                Hi 
-                            </h1> 
-
-                        </header> 
-
-                        <div> 
-
-                            <textarea name="body"  
-
-                            class="w-full border border-pink-500"  
-
-                            cols="30" rows="10"  
-
-                            placeholder="give your opinion"
-                            required> 
-
-                        </textarea>
-                        @error('body')
-                        <span class="text-xs text-red-500">{{ $message }}</span>
-
-                        @enderror 
-
-                        </div> 
-
-                        <div class="flex justify-start"> 
-
-                            <button type="submit" 
-                            class="border border-black rounded-xl mx-2 px-6 py-1 font-bold bg-blue-500 text-white hover:bg-white hover:text-black">
-                            Upload
-                            </button> 
-
-                        </div> 
-
-                    </form> 
-                    @endauth
+                
 
 
-                    @foreach ($post_in_html->comments as $comment)
-                        <x-comment :comment="$comment" />
-                    @endforeach
-                    
-
-                </section>
             </article>
+            
         </main>
-
     </section>
+        
 </x-layout_pro>
